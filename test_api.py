02f4863751ae95616f5d833d052b1c427496a125
Jacob <jacobranz4@gmail.com>
2026-08-10 21:19:52 -0700

    Testing

--- test_api.py
+++ test_api.py
@@ -1,16 +1,17 @@
 import requests
 import json
+import csv
 
-class playerInfo():
+class player():
     def __init__(self, token, tag):
         self.apiToken = token
         self.playerTag = tag
         # Define configuration variables
-        apiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjcyYTI2NmE2LTg1MDctNGQ3OS05NjVkLTM5MzA4OWU4Mzc4OCIsImlhdCI6MTc4NTEwODE1Mywic3ViIjoiZGV2ZWxvcGVyL2ZlMzA3MDZmLWJkNjgtNGFjOC04ZGQ1LTFkMDVjZTBhNTFmMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ3LjE1MC4xNzEuMjIyIl0sInR5cGUiOiJjbGllbnQifV19.GFtnAg3097GwaHZSzNFJrgzNjdfKgzyRfYVWUsg-ZSiD_ykf1-oMrjkMNZIvgyHA3rOBA1p9wzLWHyPP4DpUyw"
-        playerTag = "#9209UQO2V"  # Replace with your actual clan tag
+        #apiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhkY2IyMjcxLTFlNzQtNGFjYi1iNWMwLTI3OWYxNDNjMjhjNSIsImlhdCI6MTc4NTI5ODgwMywic3ViIjoiZGV2ZWxvcGVyL2ZlMzA3MDZmLWJkNjgtNGFjOC04ZGQ1LTFkMDVjZTBhNTFmMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc2LjMzLjc2LjgxIl0sInR5cGUiOiJjbGllbnQifV19.sgfyaaIhbQOBMrvhVpM2_Wt9PADL56T7TfPS3tq3kiPEFExXwb-s9px6DT4ob2H8T3QlnNgk2CcbWGIFvaBO7A"
+        #playerTag = "#9209UQO2V"  # Replace with your actual clan tag
 
         # URL-encode the '#' character for the endpoint
-        encoded_tag = playerTag.replace("#", "%23")
+        encoded_tag = self.playerTag.replace("#", "%23")
         self.url = f"https://api.clashofclans.com/v1/players/{encoded_tag}"
 
         # Set up the authorization headers
@@ -25,15 +26,24 @@ class playerInfo():
     def _getPlayerInfo(self):
         return self.data
 
-    def getPlayerName(self):
-        try:
-            self.playerName = self.data['name']
-        except:
-            print("Something went wrong when trying to get player name!")
+    def getPlayerInfo(self):
+        print(type(self._getPlayerInfo()))
 
-        print(self.playerName)
+    def queryPlayer(self, keyList, playerDict):
+        self.keyList = keyList
+        self.playerDict = playerDict
+        self.data = {}
+        for key in self.keyList:
+            self.data[key] = self.playerDict[key]
+        return self.data
 
-player = playerInfo("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjcyYTI2NmE2LTg1MDctNGQ3OS05NjVkLTM5MzA4OWU4Mzc4OCIsImlhdCI6MTc4NTEwODE1Mywic3ViIjoiZGV2ZWxvcGVyL2ZlMzA3MDZmLWJkNjgtNGFjOC04ZGQ1LTFkMDVjZTBhNTFmMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ3LjE1MC4xNzEuMjIyIl0sInR5cGUiOiJjbGllbnQifV19.GFtnAg3097GwaHZSzNFJrgzNjdfKgzyRfYVWUsg-ZSiD_ykf1-oMrjkMNZIvgyHA3rOBA1p9wzLWHyPP4DpUyw", 
-    "#2Q2YL8VGO"
+# Instantiate class
+player = player("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhjMzFhYTEwLTRkYTYtNGJmMi05NjkyLTViODM4ODg3YmNhYyIsImlhdCI6MTc4NjQxODIyMiwic3ViIjoiZGV2ZWxvcGVyL2ZlMzA3MDZmLWJkNjgtNGFjOC04ZGQ1LTFkMDVjZTBhNTFmMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE4Ny4xMy4xNDMuMjA0Il0sInR5cGUiOiJjbGllbnQifV19._tOYShTW-u0DO58wfHpqPFKMjzqV6xkwZ1mwse4lfd2qmH8LOKLKZb21Q78xOuMTZ0CiVX6iPi3x0xeph1vOdQ", 
+    "#9209UQO2V"
     )
-player.getPlayerName()
+queryList = ["name", "tag", "role", "expLevel", "trophies", "warStars"]
+playerInfo = player._getPlayerInfo()
+data = player.queryPlayer(queryList, playerInfo)
+with open('coc_clan_data.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=playerInfo)
+    writer.writerows(data)
