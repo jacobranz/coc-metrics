46ddd08b24d3fdcb778ec33b6f5eaf83c4833379
Jacob <jacobranz4@gmail.com>
2026-08-10 21:55:53 -0700

    Adding recommended changes.

--- test_api.py
+++ test_api.py
@@ -2,48 +2,57 @@ import requests
 import json
 import csv
 
-class player():
+class Player():
     def __init__(self, token, tag):
         self.apiToken = token
         self.playerTag = tag
-        # Define configuration variables
-        #apiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhkY2IyMjcxLTFlNzQtNGFjYi1iNWMwLTI3OWYxNDNjMjhjNSIsImlhdCI6MTc4NTI5ODgwMywic3ViIjoiZGV2ZWxvcGVyL2ZlMzA3MDZmLWJkNjgtNGFjOC04ZGQ1LTFkMDVjZTBhNTFmMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc2LjMzLjc2LjgxIl0sInR5cGUiOiJjbGllbnQifV19.sgfyaaIhbQOBMrvhVpM2_Wt9PADL56T7TfPS3tq3kiPEFExXwb-s9px6DT4ob2H8T3QlnNgk2CcbWGIFvaBO7A"
-        #playerTag = "#9209UQO2V"  # Replace with your actual clan tag
 
-        # URL-encode the '#' character for the endpoint
+    def get_info(self):
         encoded_tag = self.playerTag.replace("#", "%23")
-        self.url = f"https://api.clashofclans.com/v1/players/{encoded_tag}"
+        url = f"https://api.clashofclans.com/v1/players/{encoded_tag}"
 
-        # Set up the authorization headers
-        self.headers = {
+        headers = {
             "Accept": "application/json",
             "Authorization": f"Bearer {self.apiToken}"
         }
 
-        self.response = requests.get(self.url, headers=self.headers)
-        self.data = self.response.json()
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
 
-    def _getPlayerInfo(self):
-        return self.data
+        return response.json()
 
-    def getPlayerInfo(self):
-        print(type(self._getPlayerInfo()))
+    def queryPlayer(self, fields):
+        self.playerInfo = self.get_info()
 
-    def queryPlayer(self, keyList, playerDict):
-        self.keyList = keyList
-        self.playerDict = playerDict
-        self.data = {}
-        for key in self.keyList:
-            self.data[key] = self.playerDict[key]
-        return self.data
+        return {
+            field: self.playerInfo[field]
+            for field in fields
+            if field in self.playerInfo
+        }
 
 # Instantiate class
-player = player("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhjMzFhYTEwLTRkYTYtNGJmMi05NjkyLTViODM4ODg3YmNhYyIsImlhdCI6MTc4NjQxODIyMiwic3ViIjoiZGV2ZWxvcGVyL2ZlMzA3MDZmLWJkNjgtNGFjOC04ZGQ1LTFkMDVjZTBhNTFmMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE4Ny4xMy4xNDMuMjA0Il0sInR5cGUiOiJjbGllbnQifV19._tOYShTW-u0DO58wfHpqPFKMjzqV6xkwZ1mwse4lfd2qmH8LOKLKZb21Q78xOuMTZ0CiVX6iPi3x0xeph1vOdQ", 
-    "#9209UQO2V"
-    )
-queryList = ["name", "tag", "role", "expLevel", "trophies", "warStars"]
-playerInfo = player._getPlayerInfo()
-data = player.queryPlayer(queryList, playerInfo)
-with open('coc_clan_data.csv', 'w', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=playerInfo)
-    writer.writerows(data)
+API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhjMzFhYTEwLTRkYTYtNGJmMi05NjkyLTViODM4ODg3YmNhYyIsImlhdCI6MTc4NjQxODIyMiwic3ViIjoiZGV2ZWxvcGVyL2ZlMzA3MDZmLWJkNjgtNGFjOC04ZGQ1LTFkMDVjZTBhNTFmMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE4Ny4xMy4xNDMuMjA0Il0sInR5cGUiOiJjbGllbnQifV19._tOYShTW-u0DO58wfHpqPFKMjzqV6xkwZ1mwse4lfd2qmH8LOKLKZb21Q78xOuMTZ0CiVX6iPi3x0xeph1vOdQ"
+
+playerTags = [
+    "#GOY9G8UOQ",
+    "#GQ2U8C22U"
+]
+fields = [
+    "name", 
+    "tag", 
+    "role", 
+    "expLevel", 
+    "trophies", 
+    "warStars"
+]
+
+with open("coc_clan_data.csv", "w", newline="") as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fields)
+    writer.writeheader()
+
+    for tag in playerTags:
+        player = Player(API_TOKEN, tag)
+
+        data = player.queryPlayer(fields)
+
+        writer.writerow(data)
